61ab8a4bef2a5cb8bd3dd1124cea97dea1e3ae04
hdrlux <121645096+hdrlux@users.noreply.github.com>
2023-01-06 09:17:46 +0100

    Update ValidationISOWeek.ino

--- examples/ValidationISOWeek/ValidationISOWeek.ino
+++ examples/ValidationISOWeek/ValidationISOWeek.ino
@@ -7,13 +7,14 @@
 #include <AceTimeISOWeek.h>  // https://github.com/hdrlux/AceTimeISOWeek
 
 // Startyear for validation
-int16_t Year = 2020;
+int16_t Year = 2021;
 int32_t DateNumber = LocalDate::forComponents(Year, 1, 1).toEpochDays();
 
 void setup() {           //do only once
   Serial.begin(115200);  // Set Serial Monitor to same speed
   Serial.println();      //clean-up buffer
   Serial.println("Validation CSV for ISO8601 Week Number");
+  Serial.println("DayOfWeek, dd-mm-yyyy, EpochDay, ISOYear-ISOWeek-ISODayOfWeek, reverse-EpochDay, MaxISOWeek");
 }
 
 void loop() {
@@ -44,10 +45,16 @@ void serialprint(int32_t EpochDayI) {
 }
 /* LOG example
 Validation CSV for ISO8601 Week Number
-3, 01-01-2020 , -10958, 2020-W01-3 , -10958, 53
-4, 02-01-2020 , -10957, 2020-W01-4 , -10957, 53
-5, 03-01-2020 , -10956, 2020-W01-5 , -10956, 53
-6, 04-01-2020 , -10955, 2020-W01-6 , -10955, 53
-7, 05-01-2020 , -10954, 2020-W01-7 , -10954, 53
+DayOfWeek, dd-mm-yyyy, EpochDay, ISOYear-ISOWeek-ISODayOfWeek, reverse-EpochDay, MaxISOWeek
+5, 01-01-2021 , -10592, 2020-W53-5 , -10592, 53
+6, 02-01-2021 , -10591, 2020-W53-6 , -10591, 53
+7, 03-01-2021 , -10590, 2020-W53-7 , -10590, 53
+1, 04-01-2021 , -10589, 2021-W01-1 , -10589, 52
+2, 05-01-2021 , -10588, 2021-W01-2 , -10588, 52
+3, 06-01-2021 , -10587, 2021-W01-3 , -10587, 52
+4, 07-01-2021 , -10586, 2021-W01-4 , -10586, 52
+5, 08-01-2021 , -10585, 2021-W01-5 , -10585, 52
+6, 09-01-2021 , -10584, 2021-W01-6 , -10584, 52
+7, 10-01-2021 , -10583, 2021-W01-7 , -10583, 52
 */
 //End
